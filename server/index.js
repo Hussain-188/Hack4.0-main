e18e2a4465ba0e0a8ef48a88ec4bf355b63319b7
Mohamed Hussain <mohamedhussain18820@gmail.com>
2025-07-30 02:44:41 +0530

    port changed

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 // Configure CORS
 app.use(
